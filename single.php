eb23d7feabd9aeacfed83e884107d61213867918
Nemolite <g25092011@mail.ru>
2024-05-16 07:49:14 +0300

    Комментарии

--- single.php
+++ single.php
@@ -48,12 +48,11 @@ get_header();
 					);
 				*/
 				// If comments are open or we have at least one comment, load up the comment template.
-				/* Комментарии отключены
+				
 					if (comments_open() || get_comments_number()) :
 						comments_template();
 					endif;
-					*/
-
+					
 				endwhile; // End of the loop.
 				?>
 
